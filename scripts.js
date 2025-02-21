8a2929fab85c9cb5fe4e38f410fbd9f0adbe419b
Rodrigo Lourenco <95983889+RodasaL@users.noreply.github.com>
2025-02-21 11:22:50 +0000

    Analization of the image with IA to know the main colors of the pokemon
    This analization its done with a post request to google cloud vision IA and then it returns the RGB of the main colors of the image, being the image the pokemon

--- scripts.js
+++ scripts.js
@@ -12,7 +12,11 @@ const s_atk = document.getElementById('special-attack');
 const s_def = document.getElementById('special-defense');
 const speed = document.getElementById('speed');
 const type = document.getElementById('types');
-const imageplace = document.getElementById('img-container');
+const color1Square = document.getElementById('color1');
+const color2Square = document.getElementById('color2');
+const img_F = document.getElementById("pokemonImgF");
+const img_B = document.getElementById("pokemonImgB");
+let conversionw, conversionh;
 
 buttsearch.addEventListener('click',() =>{
     event.preventDefault()
@@ -39,15 +43,17 @@ async function Poke_Search(){
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         
-        const data = await response.json();
-        console.log('PASSOU')
+    const data = await response.json();
+    console.log('PASSOU')
     console.log(data.name);
     pk.textContent  = `${data.name}`;
     console.log(data.id);
     console.log('api call');
     pkid.textContent  = `#${data.id}`;
-    w.textContent  = `Weight: ${data.weight} Kg`;
-    h.textContent  = `Height: ${data.height} Ft`;
+    weight(data.weight);
+    height(data.height);
+    w.textContent  = `Weight: ${conversionw} Kg`;
+    h.textContent  = `Height: ${conversionh} m`;
     hp.textContent = `${data.stats[0].base_stat}`;
     atk.textContent = `${data.stats[1].base_stat}`;
     def.textContent = `${data.stats[2].base_stat}`;
@@ -57,12 +63,12 @@ async function Poke_Search(){
     type.textContent =`${data.types[0].type.name}`;
     const pokeSpriteF = data.sprites.front_default;
     const pokeSpriteB = data.sprites.back_default;
-    const img_F = document.getElementById("pokemonImgF");
-    const img_B = document.getElementById("pokemonImgB");
     img_F.src=pokeSpriteF;
     img_B.src=pokeSpriteB;
     img_F.style.display = "block";
     img_B.style.display = "block";
+
+    PokemonColors(pokeSpriteF);
 
 }catch(error){
     console.log(error);
@@ -71,5 +77,65 @@ async function Poke_Search(){
 
 }
 
+async function PokemonColors(img_p) {
+    const response = await fetch("http://localhost:3000/analyze", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ imageUrl: img_p })
+      });
+    
+      const data = await response.json();
+      console.log("Cores dominantes:", data)
 
+      
+      
+      color1Square.style.backgroundColor = data.color1;
+      color2Square.style.backgroundColor = data.color2;
+}
 
+function weight(weight_f){
+    
+    if(weight_f > 9){
+        conversionw = weight_f / 10;
+        conversionw.toFixed(1);
+    }
+    else if(weight_f > 99){
+        conversionw = weight_f / 100;
+        conversionw.toFixed(1);
+    }
+    else if(weight_f > 999){
+        conversionw = weight_f / 1000;
+        conversionw.toFixed(1);
+    }
+    else if(weight_f > 9999){
+        conversionw = weight_f / 10000;
+        conversionw.toFixed(1);
+    }
+    else{
+        conversionw = weight_f;
+    }
+    
+}
+
+function height(height_f){
+
+    if(height_f > 9){
+        conversionh = height_f / 10;
+        conversionh.toFixed(1);
+    }
+    else if(height_f > 99){
+        conversionh = height_f / 100;
+        conversionh.toFixed(1);
+    }
+    else if(height_f > 999){
+        conversionh = height_f / 1000;
+        conversionh.toFixed(1);
+    }
+    else if(height_f > 9999){
+        conversionh = height_f / 10000;
+        conversionh.toFixed(1);
+    }
+    else{
+        conversionh = height_f;
+    }
+}
